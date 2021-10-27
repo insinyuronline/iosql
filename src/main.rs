a0623b52c5f0188788bd9a50278de41ad9dd352f
Giovanni Sakti <giosakti@gmail.com>
2021-10-27 13:51:54 +0000

    feat: implement a (very) simple REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("db > ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        if input.trim().eq(".exit") {
+            break;
+        } else {
+            println!("Your command was: {}", input);
+        }
+    }
 }
