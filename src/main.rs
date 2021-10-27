30854f1fd0d3af38ceefd97e27ac0f4e1be2f01f
Giovanni Sakti <giosakti@gmail.com>
2021-10-27 14:17:30 +0000

    feat: create ability to parse and execute statement

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,16 @@ fn main() {
                 }
             }
         }
+
+        let statement = match parse_statement(&input) {
+            Ok(v) => v,
+            Err(_) => {
+                println!("Unrecognized command: {}", input);
+                continue
+            },
+        };
+
+        let _ = execute_statement(statement);
     }
 }
 
@@ -29,9 +39,35 @@ enum MetaCommand {
     Exit
 }
 
+enum Statement {
+    INSERT,
+    SELECT,
+}
+
 fn parse_meta_command(input: &str) -> Result<MetaCommand, ()> {
     match input {
         ".exit" => Ok(MetaCommand::Exit),
         _ => Err(())
+    }
+}
+
+fn parse_statement(input: &str) -> Result<Statement, ()> {
+    match input.to_lowercase().split_whitespace().next() {
+        Some("insert") => Ok(Statement::INSERT),
+        Some("select") => Ok(Statement::SELECT),
+        _ => Err(())
+    }
+}
+
+fn execute_statement(statement: Statement) -> Result<(), ()> {
+    match statement {
+        Statement::INSERT => {
+            println!("Insert!");
+            Ok(())
+        },
+        Statement::SELECT => {
+            println!("Select!");
+            Ok(())
+        }
     }
 }
